c9069de89fc3e7362a18a452b6cebecc72e5cee6
danicaldass <163008312+danicaldass@users.noreply.github.com>
2026-08-20 12:41:17 -0300

    mais mais

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 const numeroSenha = document.querySelector (".parametro-senha___texto")
-numeroSenha.textContent = 5;
+
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll(document".parametro-senha___botao")
+
+
+botoes[1].onclick = diminuir;
+
+function diminuir() {
+    tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+botoes[1].onclick = aumentar;
+
+function aumentar() {
+    tamanhoSenha++;
+    numeroSenha.textContent = tamanhoSenha;
+}
